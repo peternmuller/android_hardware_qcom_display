d9353d820a5226f6333b6374ddda0d52ae11f038
SamarV-121 <samarvispute121@pm.me>
2022-11-29 10:11:24 +0545

    display: Remove obsolete debug.sf.disable_backpressure
    * It has been removed in Android S
    * https://android.googlesource.com/platform/frameworks/native/+/2ec5391acfc340c2e195d573cf5a078ff0f280d2

--- config/display-product.mk
+++ config/display-product.mk
@@ -133,7 +133,6 @@ endif
 
 ifeq ($(TARGET_BOARD_PLATFORM),kona)
 PRODUCT_PROPERTY_OVERRIDES += \
-    debug.sf.enable_gl_backpressure=1 \
     debug.sf.enable_advanced_sf_phase_offset=1 \
     debug.sf.high_fps_late_sf_phase_offset_ns=-4000000 \
     debug.sf.high_fps_early_phase_offset_ns=-4000000 \
