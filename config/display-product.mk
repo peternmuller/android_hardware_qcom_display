3a9a7579c5c341ce949cd86248c2b7f28a24fc56
peternmuller <pm563838@gmail.com>
2025-08-20 21:02:28 +0200

    display: Convert gralloc.qcom to blueprint
    Ref: https://github.com/LineageOS/android_hardware_qcom_display/commit/34cd44131804370d44118578d4904d3637de4620
    
    Signed-off-by: peternmuller <pm563838@gmail.com>

--- config/display-product.mk
+++ config/display-product.mk
@@ -8,7 +8,6 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
     vendor.qti.hardware.display.allocator-service \
     vendor.qti.hardware.display.composer-service \
-    gralloc.$(TARGET_BOARD_PLATFORM) \
     hwcomposer.$(TARGET_BOARD_PLATFORM) \
     libsdmcore \
     libsdmutils \
